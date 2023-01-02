da25fe5468c54dbe18d43db7d950267567d5cf4a
Tomas Bagliani <tomybagliani@gmail.com>
2023-01-02 17:03:19 +0100

    add getProductsBySearch and addProduct in services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-dangle */
+
 // EndPoints
 
 // localhost:5000/api/products/search?term=sin -- Query
@@ -13,6 +14,56 @@ export const getAllProducts = async () => {
     const resp = await fetch(`${baseURL}/${collection}/`);
     const data = await resp.json();
     return data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const getProductsBySearch = async (term) => {
+  // localhost:5000/api/products/search?term=mampara
+  const baseURL = process.env.REACT_APP_API_URL;
+  const collection = 'api/products';
+  const resource = 'search?term';
+  try {
+    const resp = await fetch(`${baseURL}/${collection}/${resource}=${term}`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const addProduct = async (
+  productName,
+  description,
+  imageURL,
+  isFavourite,
+  price,
+  section
+) => {
+  const product = {
+    productName,
+    description,
+    imageURL,
+    isFavourite,
+    price,
+    section,
+  };
+
+  // localhost:5000/api/products/  -POST
+  const baseURL = process.env.REACT_APP_API_URL;
+  const collection = 'api/products';
+
+  try {
+    const resp = await fetch(`${baseURL}/${collection}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(product),
+    });
+    const result = resp.json();
+    return result;
   } catch (error) {
     return error.message;
   }
