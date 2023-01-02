18606fa300ee75160b53c9117c171e16ba3d2bb9
Tomas Bagliani <tomybagliani@gmail.com>
2023-01-02 17:18:28 +0100

    add Routes to AppRouter.js

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddProduct from '../components/AddProduct/AddProduct';
+import Header from '../components/Header/Header';
+import ProductsMain from '../components/ProductsMain/ProductsMain';
 
 export function AppRouter() {
   return (
-    <div>
-      <h1>AppRouter</h1>
+    <div className="AppRouter">
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<ProductsMain />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
