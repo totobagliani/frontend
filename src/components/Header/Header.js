b2704e55e31f0499b5325d0d13fd58f58abad044
Tomas Bagliani <tomybagliani@gmail.com>
2023-01-02 18:34:17 +0100

    add navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  SearchOutlined,
+  HomeOutlined,
+  FileAddOutlined,
+} from '@ant-design/icons';
+import styles from './styles.module.scss';
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className={styles['header-container']}>
+      <header className={styles['App-header']}>
+        <h1 className={styles.title}>Products Searcher</h1>
+        <nav className={styles['App-header__nav']}>
+          <ul className={styles['App-header__list']}>
+            <li className={styles['App-header__item']}>
+              <Link to="/" className={styles['App-header__link']}>
+                <p className={styles['App-header__text']}>HomePage</p>
+                <HomeOutlined className={styles['App-header__icon']} />
+              </Link>
+            </li>
+            <li className={styles['App-header__item']}>
+              <Link to="/addproduct" className={styles['App-header__link']}>
+                <p className={styles['App-header__text']}>Add Product</p>
+                <FileAddOutlined lassName={styles['App-header__icon']} />
+              </Link>
+            </li>
+            <li className={styles['App-header__item']}>
+              <p className={styles['App-header__text']}>SearchBar</p>
+              <SearchOutlined className={styles['App-header__icon']} />
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './styles.module.scss';
 // import { useSelector } from 'react-redux';
 import useSelectByKey from '../../hooks/useSelectByKey';
 import { PRODUCTS_STATE } from '../../redux/reducers/product.reducer';
@@ -7,7 +8,7 @@ export default function Products() {
   //  const { productAdded } = useSelector((state) => state.products);
   const productAdded = useSelectByKey(PRODUCTS_STATE.PRODUCT_ADDED);
   return (
-    <div>
+    <div className={styles['products-container']}>
       <h1>Products</h1>
       <p>
         {productAdded.productName} {productAdded.description}
